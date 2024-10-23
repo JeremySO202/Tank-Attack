e5d30481bfb3a61b1eed7892a6891b12ad6cf57f
JeremySO202 <jeremyserracin@gmail.com>
2024-10-22 22:00:28 -0600

    Fix merge

--- cmake-build-debug/CMakeFiles/Proyecto2.dir/Objetos/Objeto.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Proyecto2.dir/Objetos/Objeto.cpp.o: \
- /home/jeremy/Codigos/Tarea/Datos\ 2/Tank-Attack/Objetos/Objeto.cpp \
- /usr/include/stdc-predef.h \
- /home/jeremy/Codigos/Tarea/Datos\ 2/Tank-Attack/Objetos/Objeto.h
--- cmake-build-debug/CMakeFiles/Proyecto2.dir/Pathfinding/ListaPrioridad.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Proyecto2.dir/Pathfinding/ListaPrioridad.cpp.o: \
- /home/jeremy/Codigos/Tarea/Datos\ 2/Tank-Attack/Pathfinding/ListaPrioridad.cpp \
- /usr/include/stdc-predef.h \
- /home/jeremy/Codigos/Tarea/Datos\ 2/Tank-Attack/Pathfinding/ListaPrioridad.h
